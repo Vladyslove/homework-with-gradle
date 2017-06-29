916be132183a2a4a8c0b282324939ae49c019ba3
Vladyslove <od.vladyslav@gmail.com>
2017-06-29 16:04:17 +0300

    change only for my training4

--- homework/lesson_13/acceptanceTests/src/AcceptanceTests.java
+++ homework/lesson_13/acceptanceTests/src/AcceptanceTests.java
@@ -44,7 +44,7 @@ public class AcceptanceTests {
 
     assertThat(index, is(1));
   }
-  
+
   @Test
   public void test_BubbleSortBinarySearch_WithUnSortedArray() {
     BinarySearch bin = new BubbleSortBinarySearch();
